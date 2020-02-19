560f73b4c14be54425e34c7e5b33404f399a837e
bleek42 <brandonleek42@gmail.com>
2020-02-18 20:10:53 -0500

    findbyid and additem in indexjs

--- scripts/store.js
+++ scripts/store.js
@@ -4,8 +4,17 @@ import item from './item.js';
 const items = [];
 const hideCheckedItems = false;
 
+function findById(id){
+  return this.items.find(item => item.id === id);
+}
+
+function addItem(name){
+  try{
+    name === this.items.
+  }
+}
+
 export default {
   items,
   hideCheckedItems
 };
-
